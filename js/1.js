be21cf26d3bf5003d497cdd4dc37788dbcbbe335
Tuco Benedicto Pacifico Juan Maria Ramirez <truewerassad@gmail.com>
2020-12-05 14:40:09 +0100

    first day

---
+++ js/1.js
@@ -0,0 +1,15 @@
+// First task
+const lines = $("pre").innerHTML.split('\n');
+for (let i = 0;i < lines.length;i++)
+	for(let j = i + 1; j < lines.length;j++)
+		if (parseInt(lines[i]) + parseInt(lines[j]) == 2020)
+			console.log("Result: " + lines[i] + " * " + lines[j] + " = " + (parseInt(lines[i]) * parseInt(lines[j])));
+
+// Seconds task
+const lines = $("pre").innerHTML.split('\n');
+for (let i = 0;i < lines.length;i++)
+	for(let j = i + 1; j < lines.length;j++)
+		if (parseInt(lines[i]) + parseInt(lines[j]) < 2020)
+			for(let k = j + 1; k < lines.length;k++)
+				if (parseInt(lines[i]) + parseInt(lines[j]) + parseInt(lines[k]) == 2020)
+					console.log("Result: " + lines[i] + " * " + lines[j] + " * " + lines[k] + " = " + (parseInt(lines[i]) * parseInt(lines[j]) * parseInt(lines[k])));
